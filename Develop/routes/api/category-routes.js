d3f0e6e270fef5ceb1aba5e4a52958219809bdd8
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-04-03 17:11:21 -0700

    Debugging functions in category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -15,11 +15,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  Category.findOne();
-
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value that includes its associated Products
+  try {
+    const categoryData = await Category.findOne({
+      where: [{ id: req.params.id }],
+    },
+    {
+      include: [{ model: Product }],
+    }
+    );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
@@ -41,11 +50,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = await Category.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
+    const categoryData = await Category.destroy({where: {id: req.params.id,},});
 
     if (!categoryData) {
       res.status(404).json({ message: 'No Category found with that id!' });
--- README.md
+++ README.md
@@ -24,10 +24,13 @@ Without these guidances, my application would not exist
 
 * [findByPk: Using findByPk and WHERE condition in sequelize](https://stackoverflow.com/questions/59111392/using-findbypk-and-where-condition-in-sequelize)
 * [findByPk](https://www.tabnine.com/code/javascript/functions/sequelize/Model/findByPk)
+* [Sequelize Model Querying Basic](https://sequelize.org/docs/v6/core-concepts/model-querying-basics/)
+* [Sequelize Model Querying Finders](https://sequelize.org/docs/v6/core-concepts/model-querying-finders/)
+* [Sequelize Associations](https://sequelize.org/docs/v6/core-concepts/assocs/)
 
 Packages
 * [Mysql2](https://www.npmjs.com/package/mysql2)
-*[Sequelize](https://www.npmjs.com/package/sequelize)
+* [Sequelize](https://www.npmjs.com/package/sequelize)
 
 * Bootcamp course 13-ORM
     * 13-Ins_Validation
